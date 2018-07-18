26bd5cdb34f0b4f2cfb9f3ec3a4757c578b5ddbe
salman <ovvi@outlook.com>
2018-07-18 04:12:28 +0000

    ClassWork7

--- Server.js
+++ Server.js
@@ -6,13 +6,22 @@ var server = http.Server(app);
 app.get('/', function(req,res){
   res.sendFile(__dirname+'/index.html');
   
-  
 });
 app.get('/about', function(req,res){
   res.sendFile(__dirname+'/about.html');
   
+});
+app.get('/form', function(req,res){
+  res.sendFile(__dirname+'/form.html');
+  
+});
+app.post('/submit_user', function(req,res){
+ console.log(req.body);
   
 });
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
   });
+
+
+
