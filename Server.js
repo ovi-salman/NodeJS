a1909a8b06fd081016c15ca40f38ab9667f15fd1
salman <ovvi@outlook.com>
2018-07-18 04:46:53 +0000

    classwork7

--- Server.js
+++ Server.js
@@ -1,6 +1,9 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 var server = http.Server(app);
 
 app.get('/', function(req,res){
